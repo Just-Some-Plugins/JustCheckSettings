9c3dfe687e4570acb26301a56882107ffdb2c3d8
Ethan Henderson <ethan@zbee.codes>
2025-11-19 22:19:23 -0700

    feat: Upgrade UI to use a table

--- JustCheckSettings/UI/ChangesDisplay.cs
+++ JustCheckSettings/UI/ChangesDisplay.cs
@@ -1,7 +1,9 @@
 ﻿#region
 
+using System;
 using System.Numerics;
 using Dalamud.Bindings.ImGui;
+using Dalamud.Interface.Utility.Raii;
 using Dalamud.Interface.Windowing;
 using JustCheckSettings.Struct;
 
@@ -11,32 +13,26 @@ namespace JustCheckSettings.UI;
 
 public class ChangesDisplay : Window
 {
-    public ChangesDisplay() : base("Just Check Settings")
+    public ChangesDisplay() : base("Just Check Settings##justCheckSettings")
     {
         Flags = ImGuiWindowFlags.AlwaysAutoResize;
     }
 
     public override void Draw()
     {
+        ImGui.Dummy(new Vector2(0, 10));
+
         if (SettingChanges.Count < 1)
         {
-            ImGui.Text("No Setting Changes Detected");
+            ImGui.Dummy(new Vector2(175, 10));
+            ImGui.Text("No Setting Changes Detected Yet.");
+            ImGui.Dummy(new Vector2(0, 10));
             return;
         }
 
-        ImGui.Dummy(new Vector2(0, 10));
-
         ImGui.Text("Most Recent Setting Changes:");
-        SettingChanges.GetLatestChanges().Draw();
 
-        if (SettingChanges.Count > 1)
-        {
-            ImGui.Dummy(new Vector2(0, 10));
-            ImGui.Text("Previous Setting Changes:");
-
-            foreach (var (key, changes) in SettingChanges.GetPreviousChanges())
-                changes.Draw(key);
-        }
+        SetupTable(SettingChanges.Draw);
 
         ImGui.Dummy(new Vector2(0, 10));
 
@@ -46,6 +42,27 @@ public class ChangesDisplay : Window
             SettingChanges.Clear();
 
         ImGui.Dummy(new Vector2(0, 10));
+    }
+
+    private void SetupTable(Action tableContents)
+    {
+        using var table = ImRaii.Table("##changesTable", 5,
+            ImGuiTableFlags.SizingStretchProp);
+        if (!table)
+        {
+            ImGui.Text("COULD NOT DRAW TABLE");
+            return;
+        }
+
+        ImGui.TableSetupColumn("Time", initWidthOrWeight: 1);
+        ImGui.TableSetupColumn("Setting", initWidthOrWeight: 4);
+        ImGui.TableSetupColumn("Old Value", initWidthOrWeight: 1);
+        ImGui.TableSetupColumn("New Value", initWidthOrWeight: 1);
+        ImGui.TableSetupColumn("", ImGuiTableColumnFlags.WidthFixed, 100);
+
+        ImGui.TableHeadersRow();
+
+        tableContents();
     }
 
     public void Open()
